efc204e563e38948ce09859664871a283dfcd143
David Eliason <david@davethemaker.com>
2018-06-27 18:06:00 -0700

    update App component with denoting the platform, generic welcome. Static at this point

--- App.js
+++ App.js
@@ -12,11 +12,9 @@ import {
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const platform = Platform.select({
+  ios: 'IOS',
+  android: 'Android',
 });
 
 type Props = {};
@@ -25,13 +23,13 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Star Wars
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+        <Text style={styles.platform}>
+          Good stuff here
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
+        <Text style={styles.platform}>
+          {platform}
         </Text>
       </View>
     );
--- README.md
+++ README.md
@@ -4,3 +4,12 @@
 
 ### Description
 This is an app which fetches data from an API and renders them using React Native. This builds on top of the knowledge learned from [Star Wars Info API REact](https://github.com/davideliason/starwarsinfo_api_react_redux) but learning React Native.
+
+### Lessons Learned
+
+
+### To Use
+
+
+##### Misc Notes
+- during dev, if it crashes, $ npm start
